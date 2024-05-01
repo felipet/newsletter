874ac4c8634eb5cc382d2939de2aad2226361380
Felipe Torres González <torresfelipex1@gmail.com>
2024-05-01 11:08:51 +0200

    Integration tests broken down into smaller parts

---
+++ tests/api/subscriptions.rs
@@ -0,0 +1,85 @@
+use crate::helpers::spawn_app;
+
+#[actix_web::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let test_app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let body = "name=jane%20doe&email=jane_doe%40mail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &test_app.address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email,name FROM subscriptions",)
+        .fetch_one(&test_app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "jane_doe@mail.com");
+    assert_eq!(saved.name, "jane doe");
+}
+
+#[actix_web::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let test_app = spawn_app().await;
+    let app_address = test_app.address;
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=jane%20doe", "missing the email"),
+        ("email=jane_doe%40mail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
+
+#[actix_web::test]
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
+    let test_app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=jane_doe%40mail.com", "empty name"),
+        ("name=Jane&email=", "empty email"),
+        ("name=Jane&email=not-an-email", "invalid email"),
+        ("name=Jane-&email=not-an-email", "invalid name"),
+    ];
+
+    for (body, description) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &test_app.address))
+            .header("Content-Type", "application/x-www/form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not return a 400 Bad Request when the payload was {}",
+            description
+        );
+    }
+}

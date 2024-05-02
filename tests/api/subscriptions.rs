c788d16f769c756c3e3d9fda5ad73aee64796895
Felipe Torres González <torresfelipex1@gmail.com>
2024-05-02 11:55:56 +0200

    Set new subscritpions's status to pending and test it

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -18,14 +18,32 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let response = test_app.post_subscriptions(body.into()).await;
 
     assert_eq!(200, response.status().as_u16());
+}
 
-    let saved = sqlx::query!("SELECT email,name FROM subscriptions",)
+#[actix_web::test]
+async fn subscribe_persist_the_new_subscribe() {
+    // Prepare
+    let test_app = spawn_app().await;
+
+    let body = "name=jane%20doe&email=jane_doe%40mail.com";
+
+    // Mock server.
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&test_app.email_server)
+        .await;
+
+    test_app.post_subscriptions(body.into()).await;
+
+    let saved = sqlx::query!("SELECT email,name,status FROM subscriptions",)
         .fetch_one(&test_app.db_pool)
         .await
         .expect("Failed to fetch saved subscription");
 
     assert_eq!(saved.email, "jane_doe@mail.com");
     assert_eq!(saved.name, "jane doe");
+    assert_eq!(saved.status, "pending_confirmation");
 }
 
 #[actix_web::test]
